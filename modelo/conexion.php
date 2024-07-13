93fe5d515f8260aa22f862526754de991c807bab
MatiasSalazar1029 <matiassalazar1029@gmail.com>
2024-07-13 15:52:28 +0200

    cambios

---
+++ modelo/conexion.php
@@ -0,0 +1,14 @@
+<?php
+
+//La variable "conn" permitirá crear una conexión con nnuestra BD.
+$conn = mysqli_connect('localhost','root','','imfca');
+
+// Verificar si la conexión fue exitosa
+if (!$conn) {
+  echo "Error en la conexion a la base de datos: ";
+  exit();
+}
+
+// Establecer la codificación de caracteres
+mysqli_set_charset($conn, "utf8");
+?>
